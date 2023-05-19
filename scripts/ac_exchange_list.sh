b8c0a654a92088005994fadd03423ae46118cac2
Benjamin Yang <b98204032@gmail.com>
2023-05-19 18:28:19 +0800

    Modify the progress bar.

--- scripts/ac_exchange_list.sh
+++ scripts/ac_exchange_list.sh
@@ -1,24 +1,10 @@
 #!/bin/bash
 #
 #
-BAR_SIZE=40
-BAR_CHAR_DONE="#"
-BAR_CHAR_TODO="-"
-PERCENTAGE_SCALE=2
-#
-function show_progress {
-	# Calculate the progress in percentage
-	PERCENT=$(bc <<< "scale=${PERCENTAGE_SCALE}; 100 * ${1} / ${2}")
-	# The number of done and todo characters
-	DONE=$(bc <<< "scale=0; ${BAR_SIZE} * ${PERCENT} / 100")
-	TODO=$(bc <<< "scale=0; ${BAR_SIZE} - ${DONE}")
-	# Build the done and todo sub-bars
-	DONE_BAR=$(printf "%${DONE}s" | tr " " "${BAR_CHAR_DONE}")
-	TODO_BAR=$(printf "%${TODO}s" | tr " " "${BAR_CHAR_TODO}")
-
-	# Output the bar
-	echo -ne "\rProgress : [${DONE_BAR}${TODO_BAR}] ${PERCENT}%"
-}
+WORKING_DIR=/root/working
+TMP_DIR=${WORKING_DIR}/tmp
+SRC_DIR=${WORKING_DIR}/data_src
+RES_DIR=${WORKING_DIR}/archive_res
 #
 if [ $# -ne 1 ]
 then
@@ -29,28 +15,26 @@ fi
 INPUT_LIST=${1}
 TOTAL_LINE=`cat ${INPUT_LIST} | wc -l`
 CURRENT_LINE=1
-WORKING_DIR=/root/working
-TMP_DIR=${WORKING_DIR}/tmp
-SRC_DIR=${WORKING_DIR}/data_src
-RES_DIR=${WORKING_DIR}/archive_res
 #
 while read line
 do
+	# Calculate the progress in percentage
+	let PERCENT=(${CURRENT_LINE}*100/${TOTAL_LINE}*100)/100
 	line_a=(${line})
 	arcfile=${line_a[0]}
 	arcfile=${arcfile//[[:space:]]/}
 	filepath=${SRC_DIR}/EVENTS_NTU/${arcfile:0:6}
 	if [ ${arcfile} ] && [ -e "${filepath}/${arcfile}" ]; then
-		echo "Found the archived SAC file: ${arcfile}!"
-		echo "Start to process the SAC file: ${arcfile}..."
+		echo "[${PERCENT}%] Found the archived SAC file: ${arcfile}!"
+		echo "[${PERCENT}%] Start to process the SAC file: ${arcfile}..."
 		#
-		echo "Moving the archived SAC file to local..."
+		echo "[${PERCENT}%] Moving the archived SAC file to local..."
 		cp -R ${filepath}/${arcfile} ${TMP_DIR}
-		echo "Decompressing the archived SAC files..."
+		echo "[${PERCENT}%] Decompressing the archived SAC files..."
 		lbzip2 -dc ${TMP_DIR}/${arcfile} | tar -C ${TMP_DIR} -x
 		rm -f ${TMP_DIR}/${arcfile}
 		#
-		echo "Exchanging the Z & E components of those stations on the list..."
+		echo "[${PERCENT}%] Exchanging the Z & E components of those stations on the list..."
 		arcfolder=${TMP_DIR}/${arcfile:0:19}
 		stations=${line_a[@]:1}
 		for station in ${stations}
@@ -70,18 +54,17 @@ do
 			fi
 		done
 		#
-		echo "Compressing all the archived SAC files..."
+		echo "[${PERCENT}%] Compressing all the archived SAC files..."
 		cd ${TMP_DIR} 1> /dev/null; tar -cf ${arcfile} --use-compress-program=lbzip2 ${arcfile:0:19}; cd - 1> /dev/null
 		mv ${TMP_DIR}/${arcfile} ${RES_DIR}
 		#
-		echo "Deleting the temporary folder..."
+		echo "[${PERCENT}%] Deleting the temporary folder..."
 		rm -rf ${arcfolder}
-		echo "Finish process the archived SAC file: ${arcfile}!"
+		echo "[${PERCENT}%] Finish process the archived SAC file: ${arcfile}!"
 	else
-		echo "Can't find the archived SAC file: '${arcfile}'!"
+		echo "[${PERCENT}%] Can't find the archived SAC file: '${arcfile}'!"
 	fi
 	CURRENT_LINE=$((${CURRENT_LINE} + 1))
-	show_progress ${CURRENT_LINE} ${TOTAL_LINE}
 done < ${INPUT_LIST}
-echo "Finish process all the archived SAC files on the list!"
+echo "[${PERCENT}%] Finish process all the archived SAC files on the list!"
 exit 0
